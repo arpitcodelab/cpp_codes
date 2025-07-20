94a28703011d27f2ac341397aa88b102bde9fd2a
Arpit Prajapati <arpitworks7@gmail.com>
2025-07-20 15:35:22 +0530

    Added pattern  printing programs

---
+++ Patterns/leftnum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void left_num(int n ){
+    for (int i=1 ; i<=n ; i++){
+        for (int j =1 ; j<=i ; j++){
+        cout << j << " ";
+   
+        }
+     cout << endl;
+}
+    }
+   
+int main (){
+    int n ;
+    cout << "enter the number :- ";
+    cin >> n;
+    left_num(n);
+    return 0;
+}
